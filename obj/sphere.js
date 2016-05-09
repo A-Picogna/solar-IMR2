6d1f40a2c0de9c3ac4b53541c0cb12d6c2b88e36
Elektro Korobase <korobase.elektro@gmail.com>
2016-05-09 11:37:15 +0200

    Fix problèmes de variables

--- obj/sphere.js
+++ obj/sphere.js
@@ -7,7 +7,6 @@ sphere.prototype = new worldObject;
 		this.vertexPositionBuffer = buffers[0];
 		this.vertexTextureCoordBuffer = buffers[1];
 		this.vertexIndexBuffer = buffers[2];
-		// Ajout du buffer des normales
 		this.vertexNormalBuffer = buffers[3];
 	}
 
@@ -17,18 +16,19 @@ sphere.prototype = new worldObject;
 		normals = [];
 		vertices = [];
         textureCoords = [];
+
 		var nbVertice = 0;
 		var sphereVertexIndices = [];
 		var nbTriangles = 0;
 		var resLat = 0;
 		var resLongi = tetaMax/pasLong+1;
+
 		for (var lat=-90; lat <= phiMax; lat+=pasLat)
 		{
 			for (var longi=0; longi <= tetaMax; longi+=pasLong)
             {
-				vertices = vertices.concat(pol2Cart(longi, lat)); //A
-				normals = vertices.concat(pol2Cart(longi, lat)); // On teste un truc
-				//il manque le code des normales à ajouter!
+				vertices = vertices.concat(pol2Cart(longi, lat)); 
+				normals = normals.concat(pol2Cart(longi, lat)); 
 				textureCoords = textureCoords.concat([longi/tetaMax, (90+lat)/(90+phiMax)]);
 				if(longi != tetaMax)
 				{
@@ -51,12 +51,18 @@ sphere.prototype = new worldObject;
 			}
 			resLat++;
         }
-
+		
 		vertexPositionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         vertexPositionBuffer.itemSize = 3;
         vertexPositionBuffer.numItems = nbVertice;
+
+		vertexNormalBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER,vertexNormalBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+		vertexNormalBuffer.itemSize = 3;
+		vertexNormalBuffer.numItems = nbVertice;
 
 		vertexIndexBuffer = gl.createBuffer();
 		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, vertexIndexBuffer);
@@ -70,12 +76,5 @@ sphere.prototype = new worldObject;
 		vertexTextureCoordBuffer.itemSize = 2;
 		vertexTextureCoordBuffer.numItems = nbVertice;
 
-		vertexNormalBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,vertexNormalBuffer);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(normals), gl.STATIC_DRAW);
-		vertexNormalBuffer.itemSize = 1;
-		vertexNormalBuffer.numItems = vertexIndexBuffer.numItems;	
-		//il manque le code des normales à ajouter!
-		
 		return [vertexPositionBuffer, vertexTextureCoordBuffer, vertexIndexBuffer, vertexNormalBuffer];
 	}
